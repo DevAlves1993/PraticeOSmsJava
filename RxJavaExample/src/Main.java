4c4ce2dc27947908a1e44be5241f29bacdb09c84
Amani Christian <alvesamani@gmail.com>
2016-01-01 13:42:40 +0000

    create two method for object HistoricPurchase

--- RxJavaExample/src/Main.java
+++ RxJavaExample/src/Main.java
@@ -2,6 +2,8 @@ import agl.*;
 import agl.error.ServiceException;
 import agl.other.PartnerContracts;
 import agl.other.PartnerStatistics;
+import agl.other.PurchaseOrders;
+import agl.other.ServiceStatistics;
 import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func1;
@@ -61,15 +63,33 @@ public class Main
                     {
                         System.out.println(contract.getContractDescription());
                         System.out.println(contract.getService());
-                        System.out.println(contract.getServiceContracts());
                     }
                 );
+    }
+
+    private static void showDataServiceContract(RemainderSMS remainderSMS)
+    {
+        Observable.just(remainderSMS)
+                .map(RemainderSMS::getPartnerContracts)
+                .doOnNext(partnerContracts1 ->System.out.println( partnerContracts1.getPartnerId()))
+                .flatMap( partnerContracts -> Observable.from(partnerContracts.getContracts()))
+                .flatMap(contract ->  Observable.from(contract.getServiceContracts()))
+                .subscribe(
+                        s ->
+                        {
+                            System.out.println(s.getService());
+                            System.out.println(s.getAvailableUnits());
+                            System.out.println(s.getContractId());
+                            System.out.println(s.getExpires());
+                            System.out.println(s.getCountry());
+                            System.out.println(s.getScDescription());
+                        });
     }
 
     private static void showContentAndSenderAddOfSms(ResponseSMS responseSMS)
     {
         Observable.just(responseSMS)
-                .map(t -> t.getOutBoundSMSMessageRequest())
+                .map(ResponseSMS::getOutBoundSMSMessageRequest)
                 .doOnCompleted( () -> System.out.println("Task is a completed"))
                 .doOnError( (throwable) -> System.out.println("Error is occurred"))
                 .subscribe
@@ -86,43 +106,76 @@ public class Main
     public static void getSpecificContractDescr(RemainderSMS remainderSMS,String description)
     {
         Observable.just(remainderSMS)
-                .map( remain -> remain.getPartnerContracts())
+                .map(RemainderSMS::getPartnerContracts)
                 .flatMap(partnerContracts -> Observable.from(partnerContracts.getContracts()))
                 .filter(contract -> contract.getContractDescription().equals(description))
-                .map(contract1 -> contract1.getContractDescription())
-                .subscribe(s -> System.out.println(s));
+                .map(PartnerContracts.Contract::getContractDescription)
+                .subscribe(System.out::println);
     }
 
     public static void showServiceStatistic(StatisticSMS statisticSMS)
     {
         Observable.just(statisticSMS)
-                .map(statisticSMS1 -> statisticSMS1.getPartnerStatistics())
+                .map(StatisticSMS::getPartnerStatistics)
                 .flatMap(partnerStatistics -> Observable.from(partnerStatistics.getStatistics()))
-                .map(statistics -> statistics.getService())
-                .subscribe(s -> System.out.println(s));
+                .map(PartnerStatistics.Statistics::getService)
+                .subscribe(System.out::println);
                 
     }
     public static void showListCountryStatistics(StatisticSMS statisticSMS)
     {
         Observable.just(statisticSMS)
-                .map(statisticSMS1 -> statisticSMS1.getPartnerStatistics())
+                .map(StatisticSMS::getPartnerStatistics)
                 .doOnNext(partnerStatistics -> System.out.println(partnerStatistics.getPartnerId()))
                 .flatMap(partnerStatistics1 -> Observable.from(partnerStatistics1.getStatistics()))
                 .flatMap( statistics -> Observable.from(statistics.getServiceStatistics()))
-                .map(serviceStatistics -> serviceStatistics.getCountry())
-                .subscribe(s -> System.out.println(s));
+                .map(ServiceStatistics::getCountry)
+                .subscribe(System.out::println);
     }
 
     public static void showUsageCountryStatistics(StatisticSMS statisticSMS,String country)
     {
         Observable.just(statisticSMS)
-                .map(statisticSMS1 -> statisticSMS1.getPartnerStatistics())
+                .map(StatisticSMS::getPartnerStatistics)
                 .flatMap(partnerStatistics -> Observable.from(partnerStatistics.getStatistics()))
                 .flatMap(statistics -> Observable.from(statistics.getServiceStatistics()))
                 .filter(serviceStatistics -> serviceStatistics.getCountry().equals(country))
                 .flatMap(serviceStatistics1 -> Observable.from(serviceStatistics1.getCountyStatistics()))
-                .map(countryStatistics -> countryStatistics.getUsage())
-                .subscribe(s -> System.out.println(s));
+                .map(ServiceStatistics.CountryStatistics::getUsage)
+                .subscribe(System.out::println);
+    }
+
+    public static void showDataPurchaseHistoric(HistoricPurchase historicPurchase)
+    {
+        Observable.from(historicPurchase.getPurchaseOrders())
+                .subscribe
+                (
+                    purchaseOrders ->
+                    {
+                        System.out.println(purchaseOrders.getBundleDescription());
+                        System.out.println(purchaseOrders.getBundleId());
+                        System.out.println(purchaseOrders.getPurchaseOrderId());
+                        purchaseOrders.getOrderExecutionInformation();
+                    }
+                );
+    }
+
+    public static void showDataOrderExecutionInfor(HistoricPurchase historicPurchase)
+    {
+        Observable.from(historicPurchase.getPurchaseOrders())
+                .map(PurchaseOrders::getOrderExecutionInformation)
+                .subscribe
+                (
+                        orderExecutionInformation ->
+                        {
+                            System.out.println(orderExecutionInformation.getContractId());
+                            System.out.println(orderExecutionInformation.getService());
+                            System.out.println(orderExecutionInformation.getCountry());
+                            System.out.println(orderExecutionInformation.getAmount());
+                            System.out.println(orderExecutionInformation.getCurrency());
+                            System.out.println(orderExecutionInformation.getDate());
+                        }
+                );
     }
 
     public static ResponseSMS sendSMS(GenerateService service,Token token, SMSHeader smsHeader) throws IOException, ServiceException
