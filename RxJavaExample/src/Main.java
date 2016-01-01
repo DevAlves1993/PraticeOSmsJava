68c4df5ec8b9f66392a7a73ee4836e5fc214334c
Amani Christian <alvesamani@gmail.com>
2016-01-01 12:54:01 +0000

    create three new methods

--- RxJavaExample/src/Main.java
+++ RxJavaExample/src/Main.java
@@ -1,6 +1,7 @@
 import agl.*;
 import agl.error.ServiceException;
 import agl.other.PartnerContracts;
+import agl.other.PartnerStatistics;
 import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func1;
@@ -43,14 +44,14 @@ public class Main
             if(remainderSMS != null)
             {
                 showDataContract(remainderSMS);
+                getSpecificContractDescr(remainderSMS,"contract description");
             }
         }
     }
 
     private static void showDataContract(RemainderSMS remainderSMS)
     {
-        String idPartner;
-        Observable.just(remainderSMS)
+            Observable.just(remainderSMS)
                 .map( remain -> remain.getPartnerContracts())
                 .doOnNext(partnerContracts1 ->System.out.println( partnerContracts1.getPartnerId()))
                 .flatMap( partnerContracts -> Observable.from(partnerContracts.getContracts()))
@@ -79,6 +80,43 @@ public class Main
                         System.out.println(smsResponse.getSenderAddress());
                     }
                 );
+    }
+
+
+    public static void getSpecificContractDescr(RemainderSMS remainderSMS,String description)
+    {
+        Observable.just(remainderSMS)
+                .map( remain -> remain.getPartnerContracts())
+                .flatMap(partnerContracts -> Observable.from(partnerContracts.getContracts()))
+                .filter(contract -> contract.getContractDescription().equals(description))
+                .map(contract1 -> contract1.getContractDescription())
+                .subscribe(s -> System.out.println(s));
+    }
+
+    public static void showServiceStatistic(StatisticSMS statisticSMS)
+    {
+        Observable.just(statisticSMS)
+                .map(statisticSMS1 -> statisticSMS1.getPartnerStatistics())
+                .flatMap(partnerStatistics -> Observable.from(partnerStatistics.getStatistics()))
+                .map(statistics -> statistics.getService())
+                .subscribe(s -> System.out.println(s));
+                
+    }
+    public static void showCountryStatistics(StatisticSMS statisticSMS)
+    {
+        Observable.just(statisticSMS)
+                .map(statisticSMS1 -> statisticSMS1.getPartnerStatistics())
+                .doOnNext(partnerStatistics -> System.out.println(partnerStatistics.getPartnerId()))
+                .flatMap(partnerStatistics1 -> Observable.from(partnerStatistics1.getStatistics()))
+                .flatMap( statistics -> Observable.from(statistics.getServiceStatistics()))
+                .map(serviceStatistics -> serviceStatistics.getCountry())
+                .subscribe(s -> System.out.println(s));
+    }
+
+    public static void showIvoryCoastStatistic(StatisticSMS statisticSMS)
+    {
+
+
     }
 
     public static ResponseSMS sendSMS(GenerateService service,Token token, SMSHeader smsHeader) throws IOException, ServiceException
